3d2a6f219537c6d93ff0256bc91298a70d4ea892
andyokta <33550817+andyokta@users.noreply.github.com>
2021-05-29 07:33:35 +0700

    Create monero.sh

---
+++ monero.sh
@@ -0,0 +1,5 @@
+# monero
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.21/cpuminer-opt-linux.tar.gz
+tar -xvf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q2670hyua7a6nke8y28js0d3fga5afensknak56.VPS -p password=x
